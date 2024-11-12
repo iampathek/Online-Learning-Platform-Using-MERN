27d51cb57e4e3710d3b69688a87c09e445c5cc31
iampathek <pathekkondubouina@gmail.com>
2024-11-12 20:06:47 +0530

    Initial commit

---
+++ img/config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  //mongoURI: "mongodb://spectre:spectre007@ds253537.mlab.com:53537/elearning", //apurva's
+  //mongoURI: 'mongodb+srv://admin:admin@cluster0-xb8vd.gcp.mongodb.net/E-learning?retryWrites=true&w=majority',
+  //mongoURI: "mongodb+srv://pathekkondubouina:pathek@cluster0.7hnqh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  mongoURI: "mongodb+srv://pathekkondubouina:pathek@cluster0.7hnqh.mongodb.net/E-learning?retryWrites=true&w=majority&appName=Cluster0",
+
+  secretOrKey: "secret"
+};
